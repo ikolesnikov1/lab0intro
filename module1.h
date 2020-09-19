969ed95cdd19588638b3d9725e438b4fe944b2ea
ikolesnikov1 <70789333+ikolesnikov1@users.noreply.github.com>
2020-09-19 12:04:45 +0700

    Add files via upload

---
+++ module1.h
@@ -0,0 +1,10 @@
+#ifndef LAB0INTRO_MODULE1_H
+#define LAB0INTRO_MODULE1_H
+#include <string>
+
+namespace Module1
+{
+    std::string getMyName();
+}
+
+#endif
---
+++ module3.h
@@ -0,0 +1,10 @@
+#ifndef LAB0INTRO_MODULE3_H
+#define LAB0INTRO_MODULE3_H
+#include <string>
+
+namespace Module3
+{
+    std::string getMyName();
+}
+
+#endif
